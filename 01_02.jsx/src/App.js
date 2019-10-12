a119a17f902baa9f70749133f50313849a38e64d
杨军 <yangjun27@guazi.com>
2019-10-12 19:52:20 +0800

    feat:添加配置

--- 01_02.jsx/src/App.js
+++ 01_02.jsx/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.less';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    info: 'xxx2'
+  }
+  inputChange () {
+
+  }
+  
+  addHandle () {
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-content">
+          <input className="App-input" value={this.state.info} onChange={this.inputChange} />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
