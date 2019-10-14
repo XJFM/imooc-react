849ac9ae360e297680ba76bb6bf547948680f80a
杨军 <yangjun27@guazi.com>
2019-10-14 15:10:21 +0800

    feat:添加计时器

--- 01_02.jsx/src/App.js
+++ 01_02.jsx/src/App.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import './App.less';
+import './App.scss';
 
 class App extends React.Component {
   state = {
-    info: 'xxx2'
+    standingTime: 0
   }
-  inputChange () {
-
+  timeChange () {
+    setInterval(() => {
+      const current = this.state.standingTime + 1
+      this.setState({ standingTime: current })
+    }, 1000)
   }
-  
-  addHandle () {
-
+  convertTheTime (time) {
+    if (time < 60) {
+      return `${time}秒`
+    } else if (time < 60 * 60) {
+      return `${Math.floor(time/60)}分${time%60}秒`
+    } else {
+      return `你停留时间超过一小时，请刷新重新计时`
+    }
+  }
+  componentDidMount () {
+    this.timeChange()
   }
   render() {
     return (
       <div className="App">
         <div className="App-content">
-          <input className="App-input" value={this.state.info} onChange={this.inputChange} />
+          <p>
+            你在本页面停留时间：
+            <b>
+              { this.convertTheTime(this.state.standingTime) }
+            </b>
+          </p>
         </div>
       </div>
     )
