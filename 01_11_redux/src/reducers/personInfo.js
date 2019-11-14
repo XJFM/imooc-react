97f8f0b0aab95eeacc82f13f6f93900bcd15fc69
张少峰 <zhangshaofeng@zhangshaofengdeMacBook-Pro-2.local>
2019-11-14 20:55:39 +0800

    feat:remove

--- 01_11_redux/src/reducers/personInfo.js
+++
@@ -1,9 +0,0 @@
-function personInfo(state = { user: {} }, action) {
-  switch (action.type) {
-    case 'PERSONAL':
-      return action
-    default:
-      return state
-  }
-}
-export default personInfo
--- 01_11_redux/src/reducers/userInfo.js
+++
@@ -1,9 +0,0 @@
-function userInfo(state = { user: {} }, action) {
-  switch (action.type) {
-    case 'CUR_USER':
-      return action.user
-    default:
-      return state
-  }
-}
-export default userInfo
